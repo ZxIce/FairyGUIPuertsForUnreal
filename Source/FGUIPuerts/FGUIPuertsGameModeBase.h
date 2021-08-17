9b46819695fa524499192f183ab5915642676431
izzyzheng <695606786@q.com>
2021-08-17 21:32:29 +0800

    创建工程
    创建工程

---
+++ Source/FGUIPuerts/FGUIPuertsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FGUIPuertsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FGUIPUERTS_API AFGUIPuertsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
