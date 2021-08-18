e35506a6d062229f85a71b7f4a68dc4100573102
ZxIce <695606786@qq.com>
2021-08-18 18:00:16 +0800

    加载fgui

--- Content/JavaScript/UI/StartUI.js
+++ Content/JavaScript/UI/StartUI.js
@@ -1,11 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const UE = require("ue");
+const puerts_1 = require("puerts");
 class UIPanel {
 }
 exports.UIPanel = UIPanel;
+let uipanel;
 function Load() {
-    console.warn('HMR.finish', "StartUI2323");
-    return new UIPanel();
+    if (uipanel == null) {
+        uipanel = new UIPanel();
+    }
+    else {
+        uipanel.MainView.GetUIRoot().RemoveChild(uipanel.MainView);
+    }
+    let GameInstance = puerts_1.argv.getByName("GameInstance");
+    uipanel.MainView = UE.UIPackage.CreateObject("Basics", "Main", GameInstance);
+    uipanel.MainView.MakeFullScreen();
+    uipanel.MainView.SetParentToRoot();
+    return uipanel;
 }
 exports.Load = Load;
 //# sourceMappingURL=StartUI.js.map
--- TypeScript/UI/StartUI.ts
+++ TypeScript/UI/StartUI.ts
@@ -1,9 +1,23 @@
+import * as UE from 'ue'
+import {argv} from 'puerts';
 
 export class UIPanel
 {
     name: string;
+    MainView:UE.GComponent;
 }
+let uipanel:UIPanel;
 export function Load(){
-    console.warn('HMR.finish', "StartUI2323");
-    return new UIPanel();
+    if (uipanel == null) {
+        uipanel = new UIPanel();
+    }
+    else{
+        uipanel.MainView.GetUIRoot().RemoveChild(uipanel.MainView);
+    }
+    let GameInstance = argv.getByName("GameInstance") as UE.GameInstance;
+    
+    uipanel.MainView = UE.UIPackage.CreateObject("Basics", "Main", GameInstance) as UE.GComponent;
+    uipanel.MainView.MakeFullScreen();
+    uipanel.MainView.SetParentToRoot();
+    return uipanel;
 }
