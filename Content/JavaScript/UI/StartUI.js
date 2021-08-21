611819439d5f77a8cfb738203d49787a8e5f9883
ZxIce <695606786@qq.com>
2021-08-21 19:17:23 +0800

    添加点击事件

--- Content/JavaScript/UI/StartUI.js
+++ Content/JavaScript/UI/StartUI.js
@@ -5,17 +5,34 @@ class UIPanel {
 }
 exports.UIPanel = UIPanel;
 let uipanel;
+function OnClickGraph(Context) {
+    console.warn(Context);
+}
 function Load(ins) {
     if (uipanel == null) {
         uipanel = new UIPanel();
     }
     else {
+        let graph = uipanel.MainView.GetChild("btn_Graph");
+        graph.OnClick.Clear();
         uipanel.MainView.GetUIRoot().RemoveChild(uipanel.MainView);
     }
-    console.warn('Load');
+    //console.warn('Load');
+    //加载主UI
     uipanel.MainView = UE.UIPackage.CreateObject("Basics", "Main", ins);
     uipanel.MainView.MakeFullScreen();
     uipanel.MainView.SetParentToRoot();
+    //获取按钮
+    let graph = uipanel.MainView.GetChild("btn_Graph");
+    //设置按钮文本
+    //graph.SetText("!!!!!")
+    //添加点击事件
+    //graph.OnClick.Add(OnClickGraph);
+    //  graph.OnClick.Add((Context)=>{
+    //     console.warn(Context);
+    //  });
+    //let com = UE.UIPackage.CreateObject("Basics", "Component6", ins) as UE.GComponent;
+    //com.SetParent(uipanel.MainView);
     // uipanel.MainView = UE.UIPackage.CreateObject("Basics", "Demo_Button", ins) as UE.GComponent;
     // uipanel.MainView.MakeFullScreen();
     // uipanel.MainView.SetParentToRoot();
--- TypeScript/UI/StartUI.ts
+++ TypeScript/UI/StartUI.ts
@@ -7,21 +7,41 @@ export class UIPanel
     MainView:UE.GComponent;
 }
 let uipanel:UIPanel;
+function OnClickGraph(Context:UE.EventContext)
+{
+    console.warn(Context);
+}
 export function Load(ins: UE.TypeScriptGameInstance){
     
     if (uipanel == null) {
         uipanel = new UIPanel();
     }
     else{
+        let graph = uipanel.MainView.GetChild("btn_Graph") as UE.GButton;
+        graph.OnClick.Clear();
         uipanel.MainView.GetUIRoot().RemoveChild(uipanel.MainView);
     }
     
-    console.warn('Load');
+    //console.warn('Load');
+    //加载主UI
     uipanel.MainView = UE.UIPackage.CreateObject("Basics", "Main", ins) as UE.GComponent;
     uipanel.MainView.MakeFullScreen();
     uipanel.MainView.SetParentToRoot();
+    //获取按钮
+    let graph = uipanel.MainView.GetChild("btn_Graph") as UE.GButton;
+    //设置按钮文本
+    //graph.SetText("!!!!!")
+    //添加点击事件
+    //graph.OnClick.Add(OnClickGraph);
+    //  graph.OnClick.Add((Context)=>{
+    //     console.warn(Context);
+    //  });
+    //let com = UE.UIPackage.CreateObject("Basics", "Component6", ins) as UE.GComponent;
+    //com.SetParent(uipanel.MainView);
     // uipanel.MainView = UE.UIPackage.CreateObject("Basics", "Demo_Button", ins) as UE.GComponent;
     // uipanel.MainView.MakeFullScreen();
     // uipanel.MainView.SetParentToRoot();
     return uipanel;
 }
+
+
